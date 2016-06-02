4d6cdda4ebb150c1318d11cd16b64d3cabc2dcb3
huwei <530505193@qq.com>
2016-06-02 14:49:45 +0800

    修改包名和Application名称，修改环信AppKey

---
+++ src/cn/ucai/secret/activity/EditActivity.java
@@ -0,0 +1,34 @@
+package cn.ucai.secret.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends BaseActivity{
+	private EditText editText;
+
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(cn.ucai.secret.R.layout.activity_edit);
+		
+		editText = (EditText) findViewById(cn.ucai.secret.R.id.edittext);
+		String title = getIntent().getStringExtra("title");
+		String data = getIntent().getStringExtra("data");
+		if(title != null)
+			((TextView)findViewById(cn.ucai.secret.R.id.tv_title)).setText(title);
+		if(data != null)
+			editText.setText(data);
+		editText.setSelection(editText.length());
+		
+	}
+	
+	
+	public void save(View view){
+		setResult(RESULT_OK,new Intent().putExtra("data", editText.getText().toString()));
+		finish();
+	}
+}
