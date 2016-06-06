e638fb861879a348e1bf35c91227516cecbbb76f
huwei <530505193@qq.com>
2016-06-06 18:33:25 +0800

    完成秘密提醒界面布局

---
+++ src/cn/ucai/secret/activity/secret_remindActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.secret.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.ucai.secret.R;
+
+public class secret_remindActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret_remind);
+    }
+}
