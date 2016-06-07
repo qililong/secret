350acee3a354f9d238abda54d5406271d57afce0
huwei <530505193@qq.com>
2016-06-07 11:59:49 +0800

    完成私聊界面，发布消息界面，评论界面布局

---
+++ src/cn/ucai/secret/activity/meassage_chatActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.secret.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.ucai.secret.R;
+
+public class meassage_chatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meassage_chat);
+    }
+}
---
+++ src/cn/ucai/secret/activity/new_messageActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.secret.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.ucai.secret.R;
+
+public class new_messageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+    }
+}
